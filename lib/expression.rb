595e9b657e456c610215d8b6cbd1a2c3bfaffc2a
John Davison <johncdavison@gmail.com>
2014-10-14 10:06:03 -0700

    Add UndefinedVariable logic.

--- lib/expression.rb
+++ lib/expression.rb
@@ -1,17 +1,56 @@
 class UndefinedVariableError < StandardError; end
+require 'pry'
 
 class Expression
-
-  # Returns a new expression
+  attr_accessor :expr, :left, :node, :right, :tokens
   def initialize(expr)
     @expr = expr
+    @tokens = @expr.split(" ")
+    set_tree
   end
 
-  # Evaluates an expression, simplifying if possible.
-  #
-  # @param bindings [Hash] a map of variable names to concrete values
-  # @return [Expression] Returns a possibly-simplified Expression
+  def set_tree
+    @left = @tokens[0]
+    @right = @tokens[1]
+    @node = @tokens[2]
+  end
+
   def evaluate(bindings = {})
-    "Implement Expression#evaluate in #{__FILE__}"
+    undefined_variables?(bindings)
+
+  end
+  
+  def undefined_variables?(bindings)
+    var_definitions = @tokens.inject({}) do |definitions, token|
+      if is_variable? token
+        definitions[token] = bindings.any? {|k,v| k.to_s == token}
+      end
+      definitions
+    end
+    raise_var_error? var_definitions, bindings
+  end
+
+  def is_variable? var
+    var.to_s.match(/^:?[a-zA-Z]*$/)
+  end
+
+  def is_value? var
+    var.to_s.match(/^\d*$/)
+  end
+
+  def raise_var_error? var_definitions, bindings
+    if var_definitions.any? {|k,v| v == false} 
+      raise UndefinedVariableError
+    elsif bindings.empty? && has_variables?
+      raise UndefinedVariableError
+    end
+  end
+
+  def has_variables?
+    container = @expr.split(" ").inject([]) do |container, element|
+      containter.push true if is_variable? element
+      container
+    end
+    container.length >= 1
   end
 end
